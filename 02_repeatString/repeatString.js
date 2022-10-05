4da15761f2081f7aafe6df4720817946e036db3b
andrewperez <andrewprz5@gmail.com>
2022-10-04 22:12:59 -0400

    Program Done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
 
+const repeatString = function(string, num) {
+    console.log(string.repeat(num));
 };
+
+repeatString('Hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
+
