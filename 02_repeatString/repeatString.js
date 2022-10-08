9796f94908f4533d7a1b7892ea3f146958fc2819
andrewperez <andrewprz5@gmail.com>
2022-10-08 14:05:25 -0400

    Edit

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,9 @@
 
-const repeatString = function(string, num) {
-    console.log(string.repeat(num));
+const repeatString = function() {
+   
 };
 
-repeatString('Hey', 3);
+
 
 // Do not edit below this line
 module.exports = repeatString;
-
