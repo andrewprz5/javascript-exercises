84d4c0314e6fa594a3ff0ff377fb723dad80c0e8
andrewperez <andrewprz5@gmail.com>
2022-10-09 13:43:04 -0400

    Final Test Run

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,17 +1,25 @@
+const number = Math.floor(Math.random() * 1000);
 const repeatString = function(string, num) {
-    var text = '';
-    for (let i = 0; i < num; i++) {
-      text += string;
-    }
+  var text = '';
+  if (num < 0) {
+    return "ERROR";
+  }
+  for (let i = 0; i < num; i++) {
+    text += string;
+  }
 
-    return text;
+  return text;
+
 };
+  
 
 repeatString('hey', 3);
 repeatString('hey', 10);
 repeatString('hey', 1);
 repeatString('hey', 0);
 repeatString('hey', -1);
+repeatString('hey', number);
+repeatString('', 10);
 
 
 // Do not edit below this line
