05db0cb09b6fc1ac7a1a7326f96053682e2bc01f
andrewperez <andrewprz5@gmail.com>
2022-10-10 20:29:22 -0400

    Test Run

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let letters = string.split("");
+    let reverseText = '';
+    for (let i = 1; i <= string.length; i++) {
+      reverseText += '' + letters[letters.length - i];
+    }
+    return reverseText;
 };
 
+reverseString('hello');
+reverseString('hello there');
+reverseString('123! abc!');
+reverseString('');
 // Do not edit below this line
 module.exports = reverseString;
