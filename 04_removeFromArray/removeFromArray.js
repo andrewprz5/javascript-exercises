06bcfd11b225415b4f095ccfc9de58ca535a13bb
andrewperez <andrewprz5@gmail.com>
2022-10-11 22:13:32 -0400

    Program Done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    for (var i = 1; i < arguments.length; i++) {
+      if (arguments[i] < arr.length || typeof arguments[i] === 'string') {
+        const item = arr.indexOf(arguments[i]);
+        arr.splice(item, 1);
+      }
 
+    }
+    
+    console.log(arr);        
 };
+
+  removeFromArray([1,2,3,4], 3);
+  removeFromArray([1,2,3,4], 3, 2);
+  removeFromArray([1,2,3,4], 7, "tacos");
+  removeFromArray([1,2,3,4], 7, 2);
+  removeFromArray(["hey", 2, 3, "ho"], "hey", 3);
+  removeFromArray([1,2,3], "1", 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
