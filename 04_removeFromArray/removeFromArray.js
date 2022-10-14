0594f2c39e745934d0ee8405be2cdbda25523b3f
andrewperez <andrewprz5@gmail.com>
2022-10-13 20:19:07 -0400

    Test Run

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -14,6 +14,7 @@ const removeFromArray = function(arr) {
   removeFromArray([1,2,3,4], 3, 2);
   removeFromArray([1,2,3,4], 7, "tacos");
   removeFromArray([1,2,3,4], 7, 2);
+  removeFromArray([1,2,3,4], 1, 2, 3, 4);
   removeFromArray(["hey", 2, 3, "ho"], "hey", 3);
   removeFromArray([1,2,3], "1", 3);
 
