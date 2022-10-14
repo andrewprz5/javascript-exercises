ce0207758db4ed5eee94aeaa8fabd8f9b491ef1b
andrewperez <andrewprz5@gmail.com>
2022-10-13 20:24:31 -0400

    Test 2nd Redo

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,23 +1,22 @@
 const removeFromArray = function(arr) {
-    for (var i = 1; i < arguments.length; i++) {
-      if (arguments[i] < arr.length || typeof arguments[i] === 'string') {
-        const item = arr.indexOf(arguments[i]);
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 1; j < arguments.length; j++) {
+      if (arguments[j] == arr[i]) {
+        const item = arr.indexOf(arguments[j]);
         arr.splice(item, 1);
-      }
-
+        i = 0;
+      } 
     }
-    
-    return arr;
-           
+  }
+  console.log(arr);
 };
 
-  removeFromArray([1,2,3,4], 3);
-  removeFromArray([1,2,3,4], 3, 2);
-  removeFromArray([1,2,3,4], 7, "tacos");
-  removeFromArray([1,2,3,4], 7, 2);
-  removeFromArray([1,2,3,4], 1, 2, 3, 4);
-  removeFromArray(["hey", 2, 3, "ho"], "hey", 3);
-  removeFromArray([1,2,3], "1", 3);
-
+removeFromArray([1,2,3,4], 3);
+removeFromArray([1,2,3,4], 3, 2);
+removeFromArray([1,2,3,4], 7, "tacos");
+removeFromArray([1,2,3,4], 7, 2);
+removeFromArray([1,2,3,4], 1, 2, 3, 4);
+removeFromArray(["hey", 2, 3, "ho"], "hey", 3);
+removeFromArray([1,2,3], "1", 3);
 // Do not edit below this line
 module.exports = removeFromArray;
