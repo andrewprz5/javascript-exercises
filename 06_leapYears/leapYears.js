1228cc0ecb00870d9fe419dd5faa14d2fe6bd394
andrewperez <andrewprz5@gmail.com>
2022-10-17 20:52:51 -0400

    Program Done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if ((year % 4 == 0) && ((year % 100 != 0) || (year % 100 == 0 && 
+    year % 400 == 0))) {
+      console.log('' + year + " is a leap year.");
+      return true;
+    } else {
+      console.log('' + year + " is not a leap year.");
+      return false;
+    }
+  };  
 
-};
+  leapYears(1996);
+  leapYears(1997);
+  leapYears(34992);
+  leapYears(1900);
+  leapYears(1600);
+  leapYears(700);
 
 // Do not edit below this line
 module.exports = leapYears;
