7c0aa4df5e030d33f67e9951bb2d1cae592fa744
andrewperez <andrewprz5@gmail.com>
2022-10-17 21:08:05 -0400

    Program Done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,21 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  temp = (temp - 32) * .5556;
+  temp = Math.round(temp * 10) / 10;
+  return temp;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  temp = (temp * 1.8) + 32;
+  temp = Math.round(temp * 10) / 10;
+  return temp;
 };
+
+ftoc(32);
+ftoc(100);
+ftoc(-100);
+ctof(0);
+ctof(73.2);
+ctof(-10);
 
 // Do not edit below this line
 module.exports = {
